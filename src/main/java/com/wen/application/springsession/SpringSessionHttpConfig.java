11b6703369f46c9786a871dc27b7a7254b3cc170
peter tan <13798601782@139.com>
2022-09-22 22:52:55 +0800

    commit

--- src/main/java/com/wen/application/springsession/SpringSessionHttpConfig.java
+++ src/main/java/com/wen/application/springsession/SpringSessionHttpConfig.java
@@ -2,14 +2,23 @@ package com.wen.application.springsession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.session.events.SessionCreatedEvent;
+import org.springframework.session.events.SessionDeletedEvent;
+import org.springframework.session.events.SessionExpiredEvent;
+import org.springframework.util.StringUtils;
 
 @EnableRedisHttpSession(redisNamespace = "testSpringBootSession",maxInactiveIntervalInSeconds=100)
 public class SpringSessionHttpConfig {
     private static final Logger logger = LoggerFactory.getLogger(SpringSessionHttpConfig.class);
+    @Autowired
+    RedisOperationsSessionRepository messageListener;
     @Value("${redis.taskexecutor.corepoolsize}")
     private String corepoolsize;
 
@@ -44,5 +53,24 @@ public class SpringSessionHttpConfig {
         } catch (Exception e) {
             return defaultSize;
         }
+    }
+    @EventListener
+    public void onSessionExpired(SessionExpiredEvent expiredEvent) {
+        System.out.println("session expire:"+expiredEvent.getSessionId());
+        if(!StringUtils.isEmpty(expiredEvent.getSessionId())){
+            messageListener.delete(expiredEvent.getSessionId());
+        }
+    }
+
+    @EventListener
+    public void onSessionDeleted(SessionDeletedEvent deletedEvent) {
+        System.out.println("session被销毁"+deletedEvent.getSessionId());
+        if(!StringUtils.isEmpty(deletedEvent.getSessionId())){
+            messageListener.delete(deletedEvent.getSessionId());
+        }
+    }
+    @EventListener
+    public void onSessionCreate(SessionCreatedEvent createdEvent) {
+        System.out.println("session 创建"+createdEvent.getSessionId());
     }
 }
